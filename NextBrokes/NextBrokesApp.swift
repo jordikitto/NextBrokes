8e66c24287b8edc2b5eddaeb688cb8dab79ab973
Jordi Kitto <jordikitto@icloud.com>
2024-11-09 12:12:59 +1100

    Initial Commit

---
+++ NextBrokes/NextBrokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextBrokesApp.swift
+//  NextBrokes
+//
+//  Created by Jordi Kitto on 9/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct NextBrokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NextBrokesTests/NextBrokesTests.swift
@@ -0,0 +1,17 @@
+//
+//  NextBrokesTests.swift
+//  NextBrokesTests
+//
+//  Created by Jordi Kitto on 9/11/2024.
+//
+
+import Testing
+@testable import NextBrokes
+
+struct NextBrokesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
